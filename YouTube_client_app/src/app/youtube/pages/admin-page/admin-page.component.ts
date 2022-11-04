b655e9a3a3645c209da702808c121687b164d480
Andrew Pisliakov <pislakov@gmail.com>
2022-11-04 15:07:59 +0300

    update angular

--- YouTube_client_app/src/app/youtube/pages/admin-page/admin-page.component.ts
+++ YouTube_client_app/src/app/youtube/pages/admin-page/admin-page.component.ts
@@ -10,26 +10,28 @@ export class AdminPageComponent implements OnInit {
 
   myForm: FormGroup;
 
-  constructor() { 
-  }
-
-  onSubmit() {
-    console.log('Форма ушла на сервер');
-    console.log(this.myForm);
+  constructor() {
   }
 
   ngOnInit(): void {
     this.myForm = new FormGroup({
-      "title": new FormControl('Please enter a title', [Validators.required, Validators.minLength(5)]),
+      "title": new FormControl('', [Validators.required,  Validators.minLength(4)]),
       "discription": new FormControl(),
       "img": new FormControl(),
       "link": new FormControl()
     });
 
     this.myForm.valueChanges.subscribe(value => console.log(value));
-    this.myForm.statusChanges.subscribe(status => console.log(status))
+    
+    this.myForm.statusChanges.subscribe(status =>  { 
+      console.log(status) 
+    });
   }
 
- 
-
+  onSubmit() {
+    console.log('Форма ушла на сервер');
+    console.log(this.myForm);
+  }
 }
+
+
