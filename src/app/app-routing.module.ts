6680811d3698758ae62a7a48bccc3db48a5a5810
Andrew Pisliakov <pislakov@gmail.com>
2023-04-30 15:43:57 +0300

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
 
 @NgModule({
 
-  imports: [RouterModule.forRoot(routes), CommonModule],
+  imports: [RouterModule.forRoot(routes, { useHash: true }), CommonModule],
   exports: [RouterModule],
 
 })
